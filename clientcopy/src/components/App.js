f1fb084a60cc962bfa693bd1ee7df4c387b4e3ea
Annie Liang <annieliang@Annies-MacBook-Air.local>
2019-10-01 16:32:38 -0400

    changed client name

---
+++ clientcopy/src/components/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom' 
+import StreamList from './streams/StreamList'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamShow from './streams/StreamShow'
+import StreamDelete from './streams/StreamDelete'
+import StreamFind from './streams/StreamFind'
+import Header from './Header/Header'
+import history from '../history'
+import styles from './styles.module.css'
+
+const App = () => {
+    return (
+        <div className="bodyWrapper">
+        <Router history={history}>
+            <div>
+                <Header />
+                <Switch>
+                    <div className={styles.mainContentWrapper}>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/streams/new" exact component={StreamCreate} />
+                        <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                        <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                        <Route path="/streams/:id" exact component={StreamShow} />
+                        <Route path="/streams/search/:term" exact component={StreamFind} />
+                    </div>
+                </Switch>
+            </div>
+        </Router>
+        </div>
+    )
+}
+
+export default App;
---
+++ clientcopy/src/history.js
@@ -0,0 +1,3 @@
+import { createBrowserHistory } from 'history'; 
+
+export default createBrowserHistory(); //created to easily access history from actions 
